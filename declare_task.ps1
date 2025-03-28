8570eee7b5b9d9b1431745020ad1988724ef35bd
isssaq <54503655+isssaq@users.noreply.github.com>
2025-03-28 17:48:56 +0200

    Add files via upload
    Register schedule task with standard settings

---
+++ declare_task.ps1
@@ -0,0 +1,3 @@
+﻿$Sta = New-ScheduledTaskAction -Execute %Path_to_directory%\scheduled_task.bat
+$Stss = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
+Register-ScheduledTask "Clean recyclebin" -Action $Sta -Settings $Stss
