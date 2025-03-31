23a23ed83db2944628f0413e3e96dc4d096177a3
isssaq <54503655+isssaq@users.noreply.github.com>
2025-03-31 10:48:57 +0300

    Add files via upload
    Schedule task "Clean recycle bin" which starts in each day, at 7pm using Trigger option

---
+++ Test_Rbin_clean.ps1
@@ -0,0 +1,4 @@
+﻿$Sta = New-ScheduledTaskAction -Execute %Path_to_directory%\clear_task.bat
+$Stss = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
+$Stt = New-ScheduledTaskTrigger -Daily -At 7pm
+Register-ScheduledTask 'Test Rbin Clean' -Action $Sta -Settings $Stss -Trigger $Stt
